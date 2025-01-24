c74ad6a67fd7c6c077f45450cfafb9d703293b42
Nand72820 <duhgfhfdghg@gmail.com>
2025-01-24 22:16:59 +0530

    Update nand.py

--- nand.py
+++ nand.py
@@ -10,7 +10,7 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7128882627:AAENTcopUEidCv1nXqz_Mw4fRzw0mlLKXlY')
+bot = telebot.TeleBot('6436450622:AAFH3P40P6fF4Bp0ppalj1ZnpI3Mo1Qf9LY')
 
 # Admin user IDs
 admin_id = ["5894848388","1786683163","7858950584"]
